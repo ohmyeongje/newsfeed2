c26d581a15efe8d173e6e552a6913286aa3ec766
myeongje <tiqmtiqm6541@gmail.com>
2025-03-11 15:52:46 +0900

    feat:password추가

--- src/main/java/org/example/newsfeed/domain/user/dto/UserResponse.java
+++ src/main/java/org/example/newsfeed/domain/user/dto/UserResponse.java
@@ -7,9 +7,11 @@ public class UserResponse {
 
     private final Long id;
     private final String email;
+    private final String password;
 
-    public UserResponse(Long id, String email) {
+    public UserResponse(Long id, String email, String password) {
         this.id = id;
         this.email = email;
+        this.password = password;
     }
 }
