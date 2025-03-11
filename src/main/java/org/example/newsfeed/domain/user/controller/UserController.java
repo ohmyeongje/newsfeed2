1636cb15872a57a1b6a7bea836e9909ef655e535
myeongje <tiqmtiqm6541@gmail.com>
2025-03-11 20:28:31 +0900

    feat:필요 로직들 추가

--- src/main/java/org/example/newsfeed/domain/user/controller/UserController.java
+++ src/main/java/org/example/newsfeed/domain/user/controller/UserController.java
@@ -1,7 +1,12 @@
 package org.example.newsfeed.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.newsfeed.domain.auth.anotation.Auth;
+import org.example.newsfeed.domain.auth.dto.AuthUser;
+import org.example.newsfeed.domain.user.dto.UserRequest;
 import org.example.newsfeed.domain.user.service.UserService;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private UserService userService;
+
+    //자기 자신만 변경 가능
+    @PutMapping("/users")
+    public void update(
+            @Auth AuthUser authUser,
+            @RequestBody UserRequest request
+    ) {
+        userService.update(authUser, request);
+    }
 }
