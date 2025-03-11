b0fa07ba822794d8233e424c72c9841154d21444
myeongje <tiqmtiqm6541@gmail.com>
2025-03-11 15:52:13 +0900

    feat:AuthController 추가

---
+++ src/main/java/org/example/newsfeed/domain/auth/controller/AuthController.java
@@ -0,0 +1,31 @@
+package org.example.newsfeed.domain.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.newsfeed.domain.auth.dto.AuthRequest;
+import org.example.newsfeed.domain.auth.dto.AuthResponse;
+import org.example.newsfeed.domain.auth.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/auth/signup")
+    public void signup(
+            @RequestBody AuthRequest request
+            ) {
+        authService.signup(request);
+    }
+
+    @PostMapping("/auth/signin")
+    public ResponseEntity<AuthResponse> signIn(
+            @RequestBody AuthRequest request
+    ) {
+       return ResponseEntity.ok(authService.signIn(request));
+    }
+}
