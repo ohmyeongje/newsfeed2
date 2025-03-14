d47e30f42fedbaac85a1e2955f0a5d66c4c3ded9
myeongje <tiqmtiqm6541@gmail.com>
2025-03-14 09:58:45 +0900

    fix: 예외 처리 및 return 수정

--- src/main/java/org/example/newsfeed/domain/auth/service/AuthService.java
+++ src/main/java/org/example/newsfeed/domain/auth/service/AuthService.java
@@ -7,8 +7,10 @@ import org.example.newsfeed.domain.auth.dto.AuthRequest;
 import org.example.newsfeed.domain.auth.dto.AuthResponse;
 import org.example.newsfeed.domain.user.entity.User;
 import org.example.newsfeed.domain.user.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -25,27 +27,29 @@ public class AuthService {
            throw new IllegalArgumentException("이미 가입된 이메일 입니다");
        }
 
+
        //비밀번호 암호화
        String encoderPassword = passwordEncoder.encode(request.getPassword());
-       User user = new User(request.getEmail(), encoderPassword);
+       User user = new User(request.getEmail(), request.getName(), encoderPassword);
 
         //유저 저장
         userRepository.save(user);
     }
 
     @Transactional(readOnly = true)
-    public AuthResponse signIn(AuthRequest request) {
-        User user = userRepository.findByEmail(request.getEmail()).orElseThrow(
-                ()-> new IllegalArgumentException("존재하지 않는 회원입니다")
+    public AuthResponse signin(AuthRequest signinRequest) {
+
+        User user = userRepository.findByEmail(signinRequest.getEmail()).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "회원의 정보를 찾을 수 없습니다.")
         );
 
-        //비밀번호 검증
-        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 잘못되었습니다");
+        if (!passwordEncoder.matches(signinRequest.getPassword(), user.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "올바르지 않은 비밀번호입니다.");
         }
 
-        //비밀번호가 일치한 경우
-        String bearerJwt = jwtUtil.createToken(user.getId(), user.getEmail());
-        return new AuthResponse(bearerJwt);
+        String bearerJwt = jwtUtil.createToken(user.getId(), user.getEmail(), user.getName());
+        String jwt = jwtUtil.substringToken(bearerJwt);
+
+        return new AuthResponse(jwt);
     }
 }
