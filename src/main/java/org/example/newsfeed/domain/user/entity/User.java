5a1e3431f6d9d3dc6315cb11fde01a1a797156dc
myeongje <tiqmtiqm6541@gmail.com>
2025-03-14 09:56:40 +0900

    feat: name 필드 추가

--- src/main/java/org/example/newsfeed/domain/user/entity/User.java
+++ src/main/java/org/example/newsfeed/domain/user/entity/User.java
@@ -3,29 +3,38 @@ package org.example.newsfeed.domain.user.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.newsfeed.common.BaseEntity;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @Table(name = "users")
-public class User {
+public class User extends BaseEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, nullable = false)
     private String email;
 
     @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
     private String password;
 
-    public User(String email, String password) {
+    public User(String email, String name, String password) {
         this.email = email;
+        this.name = name;
         this.password = password;
     }
 
-    public void update(String email, String password) {
+    public void update(String email, String name) {
         this.email = email;
-        this.password = password;
+        this.name = name;
+    }
+    public void updatePassword(String newPassword) {
+        this.password = newPassword;
     }
 }
