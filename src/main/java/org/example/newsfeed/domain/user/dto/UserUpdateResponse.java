a3e24e9ce661cd9731ba6a998fbcae97f1b74346
myeongje <tiqmtiqm6541@gmail.com>
2025-03-14 10:10:27 +0900

    feat: UserUpdateResponse 추가

---
+++ src/main/java/org/example/newsfeed/domain/user/dto/UserUpdateResponse.java
@@ -0,0 +1,18 @@
+package org.example.newsfeed.domain.user.dto;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@RequiredArgsConstructor
+public class UserUpdateResponse {
+
+    private final Long id;
+    private final String email;
+    private final String name;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private final LocalDateTime updatedAt;
+}
