7315f4ddd9e7e0bd533f25213c4a933e4ba1f347
myeongje <tiqmtiqm6541@gmail.com>
2025-03-14 10:03:09 +0900

    fix: password -> name 수정

--- src/main/java/org/example/newsfeed/domain/user/dto/UserRequest.java
+++ src/main/java/org/example/newsfeed/domain/user/dto/UserRequest.java
@@ -1,9 +1,18 @@
 package org.example.newsfeed.domain.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@NoArgsConstructor
 public class UserRequest {
+
     private String email;
-    private String password;
+    private String name;
+
+
+
 }
