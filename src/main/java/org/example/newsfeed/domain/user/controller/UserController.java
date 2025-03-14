7b64c22af9fcb8c5d206b6210357ec4e592f77b4
myeongje <tiqmtiqm6541@gmail.com>
2025-03-14 10:02:27 +0900

    feat, fix: Valid 어노테이션 추가, 패스워드 관련 로직 추가, 맵핑에 /users/{userId} 수정

--- src/main/java/org/example/newsfeed/domain/user/controller/UserController.java
+++ src/main/java/org/example/newsfeed/domain/user/controller/UserController.java
@@ -1,10 +1,13 @@
 package org.example.newsfeed.domain.user.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.newsfeed.domain.auth.anotation.Auth;
 import org.example.newsfeed.domain.auth.dto.AuthUser;
 import org.example.newsfeed.domain.user.dto.UserRequest;
+import org.example.newsfeed.domain.user.dto.UserUpdatePasswordRequest;
 import org.example.newsfeed.domain.user.service.UserService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     //자기 자신만 변경 가능
-    @PutMapping("/users")
+    @PutMapping("/users/{userId}")
     public void update(
             @Auth AuthUser authUser,
+            @PathVariable Long userId,
             @RequestBody UserRequest request
     ) {
-        userService.update(authUser, request);
+        userService.update(authUser, userId, request);
+    }
+
+    @PutMapping("/users/{userId}/password")
+    public void updatePassword(
+            @Auth AuthUser authUser,
+            @RequestBody @Valid UserUpdatePasswordRequest request
+    ) {
+        userService.updatePassword(authUser, request);
     }
 }
