df8015519116db2133e580ade12d2b10b7fcd0db
myeongje <tiqmtiqm6541@gmail.com>
2025-03-17 19:36:51 +0900

    feat: 유저 조회 추가

--- src/main/java/org/example/newsfeed/domain/user/controller/UserController.java
+++ src/main/java/org/example/newsfeed/domain/user/controller/UserController.java
@@ -5,21 +5,21 @@ import lombok.RequiredArgsConstructor;
 import org.example.newsfeed.domain.auth.anotation.Auth;
 import org.example.newsfeed.domain.auth.dto.AuthUser;
 import org.example.newsfeed.domain.user.dto.UserRequest;
+import org.example.newsfeed.domain.user.dto.UserResponse;
 import org.example.newsfeed.domain.user.dto.UserUpdatePasswordRequest;
 import org.example.newsfeed.domain.user.service.UserService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/users/{userId}")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
     //자기 자신만 변경 가능
-    @PutMapping("/users/{userId}")
+    @PutMapping
     public void update(
             @Auth AuthUser authUser,
             @PathVariable Long userId,
@@ -28,7 +28,15 @@ public class UserController {
         userService.update(authUser, userId, request);
     }
 
-    @PutMapping("/users/{userId}/password")
+    @GetMapping
+    public ResponseEntity<UserResponse> findUser(
+            @Auth AuthUser authUser,
+            @PathVariable Long userId)
+    {
+       return ResponseEntity.ok(userService.findUser(authUser.getId(), userId));
+    }
+
+    @PutMapping("/password")
     public void updatePassword(
             @Auth AuthUser authUser,
             @RequestBody @Valid UserUpdatePasswordRequest request
