c1e580004e101ec0e02d255b448dba8547af80c9
myeongje <tiqmtiqm6541@gmail.com>
2025-03-17 19:37:08 +0900

    feat: UserResponse 추가

--- src/main/java/org/example/newsfeed/domain/user/dto/UserResponse.java
+++ src/main/java/org/example/newsfeed/domain/user/dto/UserResponse.java
@@ -1,17 +1,31 @@
 package org.example.newsfeed.domain.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponse {
 
     private final Long id;
     private final String email;
-    private final String password;
+    private final String name;
+    private final int followerUser;
+    private final int followingUser;
 
-    public UserResponse(Long id, String email, String password) {
+    public UserResponse(Long id, String email, String name, int followerUser, int followingUser) {
         this.id = id;
         this.email = email;
-        this.password = password;
+        this.name = name;
+        this.followerUser = followerUser;
+        this.followingUser = followingUser;
+    }
+
+    public UserResponse(Long id, String name, int followerUser, int followingUser) {
+        this.id = id;
+        this.email = null;
+        this.name = name;
+        this.followerUser = followerUser;
+        this.followingUser = followingUser;
     }
 }
