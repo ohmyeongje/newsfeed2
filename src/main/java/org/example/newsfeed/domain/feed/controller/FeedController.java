a2d7e25a7250550ea09b32bbd4b10725e57f822d
myeongje <tiqmtiqm6541@gmail.com>
2025-03-18 19:40:27 +0900

    feat: 뉴스피드 조회 추가

--- src/main/java/org/example/newsfeed/domain/feed/controller/FeedController.java
+++ src/main/java/org/example/newsfeed/domain/feed/controller/FeedController.java
@@ -6,12 +6,17 @@ import org.example.newsfeed.domain.auth.dto.AuthUser;
 import org.example.newsfeed.domain.feed.dto.FeedPagingResponse;
 import org.example.newsfeed.domain.feed.dto.FeedRequest;
 import org.example.newsfeed.domain.feed.dto.FeedResponse;
+import org.example.newsfeed.domain.feed.entity.Feed;
 import org.example.newsfeed.domain.feed.service.FeedService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/feeds")
@@ -39,13 +44,23 @@ public class FeedController {
         Page<FeedPagingResponse> feeds = feedService.findAll(pageRequest);
         return ResponseEntity.ok(feeds);
     }
-    
+
+    @GetMapping
+    public ResponseEntity<List<FeedResponse>> searchFeedsByDate(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+
+        List<FeedResponse> feeds = feedService.getFeedsByDateRange(startDate, endDate);
+        return ResponseEntity.ok(feeds);
+    }
+
+
     @GetMapping("/{feedId}")
     public ResponseEntity<FeedResponse> getFeed(@PathVariable Long feedId) {
         return ResponseEntity.ok(feedService.getFeed(feedId));
     }
 
-    @PutMapping("/{feedId}")
+    @PutMapping("/{feedID}")
     public ResponseEntity<FeedResponse> updateFeed(
             @Auth AuthUser authUser,
             @PathVariable Long feedID,
@@ -58,5 +73,11 @@ public class FeedController {
     public ResponseEntity<Void> deleteFeed(@Auth AuthUser authUser, @PathVariable Long feedId) {
         feedService.deleteFeed(authUser.getId(), feedId);
         return ResponseEntity.noContent().build();
+    }
+
+    // 뉴스피드 조회
+    @GetMapping("/newsfeed")
+    public ResponseEntity<List<Feed>> getNewsFeed(@PathVariable Long userId) {
+        return ResponseEntity.ok(feedService.getNewsFeed(userId));
     }
 }
