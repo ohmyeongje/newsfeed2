872a1048a11b9cecfa83a03396dbe40eb92694d3
myeongje <tiqmtiqm6541@gmail.com>
2025-03-18 19:40:53 +0900

    feat: 시간 관계 로직 추가

--- src/main/java/org/example/newsfeed/domain/feed/repository/FeedRepository.java
+++ src/main/java/org/example/newsfeed/domain/feed/repository/FeedRepository.java
@@ -1,9 +1,16 @@
 package org.example.newsfeed.domain.feed.repository;
 
 import org.example.newsfeed.domain.feed.entity.Feed;
+import org.example.newsfeed.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface FeedRepository extends JpaRepository<Feed, Long> {
+    List<Feed> findByUserInOrderByCreatedAtDesc(List<User> users);
+    List<Feed> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
+
 }
