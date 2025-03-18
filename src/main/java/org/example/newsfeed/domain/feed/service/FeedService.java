492705e62070a40c2944c895868828c6b2ff5e66
myeongje <tiqmtiqm6541@gmail.com>
2025-03-18 19:41:44 +0900

    feat: 피드 조회 추가

--- src/main/java/org/example/newsfeed/domain/feed/service/FeedService.java
+++ src/main/java/org/example/newsfeed/domain/feed/service/FeedService.java
@@ -7,6 +7,7 @@ import org.example.newsfeed.domain.feed.dto.FeedRequest;
 import org.example.newsfeed.domain.feed.dto.FeedResponse;
 import org.example.newsfeed.domain.feed.entity.Feed;
 import org.example.newsfeed.domain.feed.repository.FeedRepository;
+import org.example.newsfeed.domain.follow.service.FollowService;
 import org.example.newsfeed.domain.user.entity.User;
 import org.example.newsfeed.domain.user.repository.UserRepository;
 import org.springframework.data.domain.Page;
@@ -15,13 +16,20 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class FeedService {
 
     private final FeedRepository feedRepository;
     private final UserRepository userRepository;
+    private final FollowService followService;
 
+    //피드 생성
     @Transactional
     public FeedResponse save(Long userId, FeedRequest request) {
         User user = userRepository.findById(userId).orElseThrow(
@@ -31,12 +39,14 @@ public class FeedService {
         return new FeedResponse(feed);
     }
 
+    //피드 조회
     @Transactional(readOnly = true)
     public Page<FeedPagingResponse> findAll(Pageable pageable) {
         return feedRepository.findAll(pageable)
                 .map(FeedPagingResponse::fromEntity);
     }
 
+    //특정 피드 조회
     @Transactional(readOnly = true)
     public FeedResponse getFeed(Long feedId) {
         Feed feed = feedRepository.findById(feedId).orElseThrow(
@@ -44,6 +54,19 @@ public class FeedService {
         return new FeedResponse(feed);
     }
 
+    //날짜별 검색
+    @Transactional(readOnly = true)
+    public List<FeedResponse> getFeedsByDateRange(LocalDate startDate, LocalDate endDate) {
+        LocalDateTime startDateTime = startDate.atStartOfDay();
+        LocalDateTime endDateTime = endDate.atTime(23, 59, 59);
+
+        List<Feed> feeds = feedRepository.findByCreatedAtBetween(startDateTime, endDateTime);
+        return feeds.stream()
+                .map(FeedResponse::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    //피드 수정
     @Transactional
     public FeedResponse updateFeed(Long userId, Long feedID, FeedRequest request) {
         Feed feed = feedRepository.findById(feedID).orElseThrow(
@@ -58,6 +81,7 @@ public class FeedService {
 
     }
 
+    //피드 삭제
     @Transactional
     public void deleteFeed(Long userId, Long feedId) {
         Feed feed = feedRepository.findById(feedId)
@@ -66,5 +90,12 @@ public class FeedService {
             throw new IllegalArgumentException("권한이 없습니다");
         }
         feedRepository.delete(feed);
+    }
+
+    //뉴스피드 조회
+    @Transactional(readOnly = true)
+    public List<Feed> getNewsFeed(Long userId) {
+        List<User> followingUsers = followService.getFollowingUsers(userId);
+        return feedRepository.findByUserInOrderByCreatedAtDesc(followingUsers);
     }
 }
