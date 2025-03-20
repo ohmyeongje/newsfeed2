cfb8f74c04a572c658cdc9def521d4222453267e
myeongje <tiqmtiqm6541@gmail.com>
2025-03-20 10:27:32 +0900

    feat: 테스트에 따른 의존성 추가 추가

--- src/main/java/org/example/newsfeed/domain/user/dto/UserRequest.java
+++ src/main/java/org/example/newsfeed/domain/user/dto/UserRequest.java
@@ -1,15 +1,16 @@
 package org.example.newsfeed.domain.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserRequest {
 
     private String email;
     private String name;
-
-
-
 }
