ddd0ef9c8fee881d22200ce4190c85f86718424e
myeongje <tiqmtiqm6541@gmail.com>
2025-03-20 10:28:11 +0900

    feat: 테스트에 따른 의존성 추가

--- src/main/java/org/example/newsfeed/domain/user/entity/User.java
+++ src/main/java/org/example/newsfeed/domain/user/entity/User.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.example.newsfeed.common.BaseEntity;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users")
